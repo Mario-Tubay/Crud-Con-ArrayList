338a7572f3822330597f2925fab77d67f9f06d8a
Mario <mario-tubay@hotmail.com>
2021-01-23 19:58:14 -0500

    Modifica

--- src/deber2/Deber2.java
+++ src/deber2/Deber2.java
@@ -41,12 +41,17 @@ public class Deber2 {
                         }
                     }while(menuTipo()!= 6);
                     break;
-                case 2:
-                     //while(menuTipo()!= 6){
-                        
-                    //}
+                case 2://MODIFICACION 
+                    do {
+                        switch(menuTipo()){
+                            case 1://Modificar Estudiante
+                                modificarEstudiante();
+                                break;
+                        }
+                    } while (menuTipo()!=6);
+                    
                     break;
-                case 3:
+                case 3://ELIMINACION
                      while(menuTipo()!= 6){
                         
                     }
@@ -352,7 +357,218 @@ public class Deber2 {
         String cedula = sc.next();
         for(int i =0; i<listCons.size();i++){
             if(cedula.equals(listCons.get(i).getCedula())){
-                System.out.println(""+listCons.get(i).toString());
+                
+            }
+        }
+    }
+     public static void modificarConserje(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese la Cedula: ");
+        String cedula = sc.next();
+        for(int i =0; i<listCons.size();i++){
+            if(cedula.equals(listCons.get(i).getCedula())){
+               String nombre, apellido;
+                String fech_nac;
+                Date fech_nac1= null;
+                SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+                String carreras, turno,  fechaIngreso;
+                Date fechaIngreso1 = null;
+                int años_servicio;
+
+                System.out.print("Cedula: ");     
+                cedula= sc.next();
+                System.out.print("Nombre: ");
+                nombre= sc.next();
+                System.out.print("Apellido: ");
+                apellido = sc.next();
+                System.out.print("Fecha de Nacimiento: ");
+                fech_nac = sc.next();
+                 try {
+                fech_nac1= formato.parse(fech_nac);
+                 } catch (Exception e) {
+                 }
+
+                 System.out.print("Carrera: ");
+                 carreras = sc.next();
+                 System.out.print("Turno: ");
+                 turno = sc.next();
+               System.out.print("Fecha de Ingreso: ");
+                fechaIngreso = sc.next();
+                 try {
+                fechaIngreso1= formato.parse(fechaIngreso);
+                 } catch (Exception e) {
+                 }
+                 System.out.print("Años de servicios: ");
+                 años_servicio = sc.nextInt();
+
+                 Conserje s = new Conserje(carreras, fechaIngreso1, años_servicio, turno, cedula, nombre, apellido, fech_nac1);
+                 listCons.set(i, s);
+     
+            }
+        }
+    }
+     public static void modificarEstudiante(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese la CODIGO: ");
+        String cedula = sc.next();
+        for(int i =0; i<listEst.size();i++){
+            if(cedula.equals(listEst.get(i).getCedula())){
+                SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+                String Carrera;
+                int semestre;
+                String fecha_inicio;
+                Date fecha_inicio1= null;
+                String fecha_finalizacion, fech_nac;
+                Date fecha_finalizacion1=null, fech_nac1= null;
+                int materiaSemestre;
+                String nombre, apellido;
+                    System.out.print("Cedula: ");     
+                    cedula= sc.next();
+                    System.out.print("Nombre: ");
+                    nombre= sc.next();
+                    System.out.print("Apellido: ");
+                    apellido = sc.next();
+                    System.out.print("Fecha de Nacimiento: ");
+                    fech_nac = sc.next();
+                     try {
+                    fech_nac1= formato.parse(fech_nac);
+                     } catch (Exception e) {
+                     }
+                    System.out.print("Carrera: ");
+                    Carrera = sc.next();
+                    System.out.print("Semestre: ");
+                    semestre = sc.nextInt();
+                    System.out.print("Materia por semestre: ");
+                    materiaSemestre = sc.nextInt();
+                    System.out.print("Fecha de Inicio: ");
+                    fecha_inicio = sc.next();
+                     try {
+                    fecha_inicio1= formato.parse(fech_nac);
+                     } catch (Exception e) {
+                         System.out.println(""+e.getMessage());
+                     }
+                     System.out.println("fecha nac "+ fech_nac1);
+                       System.out.print("Fecha de Finalizacion: ");
+                    fecha_finalizacion = sc.next();
+                     try {
+                    fecha_finalizacion1 = formato.parse(fech_nac);
+                     } catch (Exception e) {
+                         System.err.println(""+e.getMessage());
+                     }
+
+                    Estudiante e = new Estudiante(Carrera, semestre, fecha_inicio1, fecha_finalizacion1, materiaSemestre, cedula, nombre, apellido, fecha_finalizacion1);
+                    listEst.set(i, e);
+                    System.out.println(""+listEst.get(i).toString());
+             }
+        }
+    }
+     public static void modificarProfesor(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese la CODIGO: ");
+        String cedula = sc.next();
+        for(int i =0; i<listPro.size();i++){
+            if(cedula.equals(listPro.get(i).getCedula())){
+
+                    SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+
+
+                    String fech_nac;
+                    Date fech_nac1= null;
+
+
+                    int cantMaterias;
+                    String Titulo;
+                    String nombre, apellido, materias, carreras;
+                        System.out.print("Cedula: ");     
+                        cedula= sc.next();
+                        System.out.print("Nombre: ");
+                        nombre= sc.next();
+                        System.out.print("Apellido: ");
+                        apellido = sc.next();
+                        System.out.print("Fecha de Nacimiento: ");
+                        fech_nac = sc.next();
+                         try {
+                        fech_nac1= formato.parse(fech_nac);
+                         } catch (Exception e) {
+                         }
+
+                         System.out.print("Materias: ");
+                         materias = sc.next();
+                         System.out.print("Carreras: ");
+                         carreras = sc.next();
+                         System.out.print("Titulo: ");
+                         Titulo = sc.next();
+                        Profesor p = new Profesor(carreras, materias, Titulo, cedula, nombre, apellido, fech_nac1);
+                        listPro.set(i, p);
+            }
+        }
+    }
+     public static void modificarMateria(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese la CODIGO: ");
+        String cedula = sc.next();
+        for(int i =0; i<listMat.size();i++){
+            if(cedula.equals(listMat.get(i).getCódigo())){
+                 String nombre;
+                 int codigo; 
+                 int semestre;
+                 System.out.print("Nombre de la Materia: ");
+                 nombre = sc.next();
+                 System.out.print("Codigo de la Materia: ");
+                 codigo = sc.nextInt();
+                 System.out.print("Semestre de la Materia: ");
+                 semestre = sc.nextInt();
+                 Materia m = new Materia(nombre, codigo, semestre);
+                listMat.set(i, m);
+
+                
+            }
+        }
+    }
+     public static void modificarSecretario(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese la CODIGO: ");
+        String cedula = sc.next();
+        for(int i =0; i<listSec.size();i++){
+            if(cedula.equals(listSec.get(i).getCedula())){
+                          String nombre, apellido;
+                          String fech_nac;
+                          Date fech_nac1= null;
+                          SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+                          String carreras, turno,  fechaIngreso;
+                          Date fechaIngreso1 = null;
+                          int años_servicio;
+
+
+                              System.out.print("Cedula: ");     
+                              cedula= sc.next();
+                              System.out.print("Nombre: ");
+                              nombre= sc.next();
+                              System.out.print("Apellido: ");
+                              apellido = sc.next();
+                              System.out.print("Fecha de Nacimiento: ");
+                              fech_nac = sc.next();
+                               try {
+                              fech_nac1= formato.parse(fech_nac);
+                               } catch (Exception e) {
+                               }
+
+                               System.out.print("Carrera: ");
+                               carreras = sc.next();
+                               System.out.print("Turno: ");
+                               turno = sc.next();
+                             System.out.print("Fecha de Ingreso: ");
+                              fechaIngreso = sc.next();
+                               try {
+                              fechaIngreso1= formato.parse(fechaIngreso);
+                               } catch (Exception e) {
+                               }
+                               System.out.print("Años de servicios: ");
+                               años_servicio = sc.nextInt();
+
+                               secretario s = new secretario(carreras, fechaIngreso1, años_servicio, turno, cedula, nombre, apellido, fech_nac1);
+                               listSec = s.ingresar(s);
+
             }
         }
     }
